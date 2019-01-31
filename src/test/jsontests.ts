8e0f29f4a0d18fac8c6315dc1385c8c5ef6d9318
johtela <tommi.johtela@dnainternet.net>
2019-01-31 16:42:37 +0200

    Improved tests.

--- src/test/jsontests.ts
+++ src/test/jsontests.ts
@@ -1,14 +1,25 @@
 import { parseJson } from "./jsonparser"
-import { readFileSync } from "fs"
+import { readFileSync, readdirSync } from "fs"
 import { expect } from "chai"
+import * as jsc from "jsverify"
 
-describe("Test simple file", () => {
-    it("should succeed parsing 'test.json'", () => {
-        let buf = readFileSync("testdata/test.json")
-        let text = buf.toString()
-        expect(text.length).greaterThan(0)
-        let json1 = parseJson(text)
-        let json2 = JSON.parse(text)
-        expect(json1).to.deep.equal(json2)
-    })
+function testParsingFile(fileName: string) {
+    let buf = readFileSync(fileName);
+    let text = buf.toString();
+    expect(text.length).greaterThan(0);
+    let json1 = parseJson(text);
+    let json2 = JSON.parse(text);
+    expect(json1).to.deep.equal(json2);
+}
+
+describe("Test JSON file parsing", () => {
+    let testdir = "testdata/"
+    let files = readdirSync(testdir)
+    for (let i = 0; i < files.length; i++) {
+        const file = testdir + files[i];
+        if (file.endsWith(".json"))
+            it(`should succeed parsing '${file}'`, () =>
+                testParsingFile(file))
+    }
 })
+
