c2ec7b690522b783aaf1d42e765719da687a6a96
johtela <tommi.johtela@dnainternet.net>
2019-02-03 21:02:12 +0200

    Cannot proceed building arbitrary expressions until letrec typings are released in jsverify.

--- src/test/exprtests.ts
+++ src/test/exprtests.ts
@@ -4,15 +4,15 @@ import * as jsc from "jsverify"
 import { ParseError } from "../error";
 
 describe("Test parsing of predefined expressions", () => {
-    let testset: [string, number][] = [
-        ["1 + -1", 0],
-        ["2 + 3 * 3", 11],
-        ["1 - 1 / 2", 0.5],
-        ["(1 - 1) / 2", 0],
-        ["(1) + (((2)) + 3)", 6]
-    ]
+    let testset: string[] = [
+        "1 + -1", 
+        "2 + 3 * 3", 
+        "1 - 1 / 2", 
+        "(1 - 1) / 2",
+        "(1) + (((2)) + 3)" ]
     for (let i = 0; i < testset.length; i++) {
-        let [expr, res] = testset[i]
+        let expr = testset[i]
+        let res = eval(expr)
         it(`expression '${expr}' should evaluate to ${res}`, () => {
             let calcres = evaluateExpression(expr);
             expect(calcres).to.equal(res);
@@ -34,3 +34,6 @@ describe("Test failing expressions", () => {
         })
     }
 })
+
+const arbNum = jsc.number.smap(n => n.toString(), s => Number(s))
+const arbOper = jsc.elements(["+", "-", "*", "/"])
